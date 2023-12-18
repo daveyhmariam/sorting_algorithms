7ac666eb340c073e2310dcebbba4c3ad1ef32668
daveyhmariam <daveyhmariam@gmail.com>
2023-12-18 19:59:58 +0300

    update bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, swap;
 
 	swap = 0;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
