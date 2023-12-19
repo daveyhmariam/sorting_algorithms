064f055f60c8ba2a76a5a95dc8ac0547a9afc78c
daveyhmariam <daveyhmariam@gmail.com>
2023-12-19 16:55:43 +0300

    update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,58 +1,48 @@
-#include "sort.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include"sort.h"
 
 /**
- * insertion_sort_list - sorting with insertion algorithm
- * @list: head to list
- * Return: Nothing
-*/
+ * insertion_sort_list - implements the insertion sort algorithm to sort
+ * a doubly linked list in ascending order.
+ *
+ * @list: The "list" parameter is a pointer to a pointer
+ * to a linked list of integers.
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *insert, *iter;
+listint_t *curr;
+listint_t *prev;
 
-	if (*list == NULL || (*list)->next == NULL)
-		return;
-	insert = (*list)->next;
-	iter = insert;
-	while (insert != NULL)
-	{
-		insert = insert->next;
-		while (iter && iter->prev)
-		{
-			if ((iter->n) < (iter->prev->n))
-			{
-				swap(iter);
-				if (iter->prev == NULL)
-					*list = iter;
-				print_list(*list);
-				if (iter->prev == NULL)
-					break;
-			}
-			else if (iter->n >= iter->prev->n)
-				break;
-
-		}
-		iter = insert;
-	}
+for (curr = (*list)->next; curr != NULL; curr = curr->next)
+{
+prev = curr->prev;
+while (prev != NULL && prev->n > curr->n)
+{
+swapper(list, &prev, curr);
+print_list((*list));
+}
+}
 }
 
 /**
- * swap - swaps nodes
- * @swp: swaping node
- * Return: Nothing
-*/
-void swap(listint_t *swp)
+ * swapper - swaps two nodes in a doubly linked list.
+ *
+ * @head: A pointer to the head of the linked list.
+ * @Node_1: Node_1 is a pointer to a pointer to a node in a doubly linked list.
+ * @Node_2: Node_2 is a pointer to a node in a doubly linked list.
+ */
+void swapper(listint_t **head, listint_t **Node_1, listint_t *Node_2)
 {
-	listint_t *tmp;
-
-	tmp = swp->prev;
-	if (tmp == NULL)
-		return;
-	tmp->next = swp->next;
-	if (tmp->next)
-		tmp->next->prev = tmp;
-	swp->next = tmp;
-	swp->prev = tmp->prev;
-	tmp->prev = swp;
-	if (swp->prev)
-		swp->prev->next = swp;
+(*Node_1)->next = Node_2->next;
+if (Node_2->next != NULL)
+Node_2->next->prev = *Node_1;
+Node_2->prev = (*Node_1)->prev;
+Node_2->next = *Node_1;
+if ((*Node_1)->prev != NULL)
+(*Node_1)->prev->next = Node_2;
+else
+*head = Node_2;
+(*Node_1)->prev = Node_2;
+*Node_1 = Node_2->prev;
 }
