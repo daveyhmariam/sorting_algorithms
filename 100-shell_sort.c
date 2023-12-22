768ef2cf563d60495c8f49c2697354cba1b93074
daveyhmariam <daveyhmariam@gmail.com>
2023-12-22 12:24:14 +0300

    update 100-shell_sort.c

---
+++ 100-shell_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+
+void shell_sort(int *array, size_t size)
+{
+	size_t i, j, knuth, tmp;
+
+	knuth = 1;
+	while (knuth <= size / 2)
+		knuth = knuth * 3 + 1;
+	for (i = 0; i < size; i++)
+		{
+			for (j = 0; (j + knuth) < size; j++)
+			{
+				if (array[j] < array[knuth])
+				{
+					tmp = array[j];
+					array[j] = array[knuth];
+					array[knuth] = tmp;
+					print_array(array, size);
+				}
+			}
+			knuth /= 3;
+		}
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j > 0; j--)
+		{
+			if (array[j] < array[j - 1])
+			{
+				tmp = array[j];
+				array[j] = array[j - 1];
+				array[j - 1] = tmp;
+			}
+		}
+	}
+}
--- sort.h
+++ sort.h
@@ -27,4 +27,5 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void lumito(int *array, int low, int high, int size);
 void swap_quick(int *a, int *b);
+void shell_sort(int *array, size_t size);
 #endif
