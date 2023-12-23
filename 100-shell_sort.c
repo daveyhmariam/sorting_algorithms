c93a2df904d2a3fe6fe71bdda969d0d9fa8485f9
daveyhmariam <daveyhmariam@gmail.com>
2023-12-23 08:40:27 +0300

    update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -3,38 +3,25 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j, knuth, tmp;
-
+	size_t  i, j, kn = 0;
+	int tmp;
 	if (!array || size < 2)
 		return;
 
-	knuth = 1;
-	while (knuth <= size / 2)
-		knuth = knuth * 3 + 1;
-	for (i = 0; i < size && knuth >= 1; i++)
-		{
-			for (j = 0; (j + knuth) < size; j++)
-			{
-				if (array[j] < array[knuth])
-				{
-					tmp = array[j];
-					array[j] = array[knuth];
-					array[knuth] = tmp;
-					print_array(array, size);
-				}
-			}
-			knuth /= 3;
-		}
-	for (i = 0; i < size; i++)
+	while (kn <= size / 3)
+		kn = kn * 3 + 1;
+	
+	for (; kn >= 1; kn /= 3)
 	{
-		for (j = i; j > 0; j--)
+	for (i = kn; i < size; i++)
+	{
+		for (j = i; j >= kn && array[j] < array[j - kn]; j -= kn)
 		{
-			if (array[j] < array[j - 1])
-			{
-				tmp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = tmp;
-			}
+			tmp = array[j];
+			array[j] = array[j - kn];
+			array[j - kn] = tmp;
+			print_array(array, size);
 		}
+	}
 	}
 }
