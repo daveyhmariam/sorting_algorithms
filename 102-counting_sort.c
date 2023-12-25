de9646e0a97c8492328eff3e6e31d24f727bb6f0
daveyhmariam <daveyhmariam@gmail.com>
2023-12-25 08:39:31 +0300

    update 101-cocktail_sort_list.c

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -1,48 +1,80 @@
 #include "sort.h"
 
+int largest(int *array, size_t size);
 
 /**
- * counting_sort - sorting using count sorting
- * @array: array to be sorted
- * @size: size of array
- * Return: Nothing
+ * counting_sort - sorts by counting elements occurence
+ *
+ * @array: the array to be sorted
+ * @size: the size of the array
+ *
+ * Return: nothing
 */
+
 void counting_sort(int *array, size_t size)
 {
-	int sz = (int) size;
-	int *freq, *sorted;
-	int i, idx, max;
+	int *count, i;
+	int len = largest(array, size), *array2;
 
-	if (size < 2)
-		return;
-	for (max = 0, i = 0; i < sz; i++)
-		if (array[i] > max)
-			max = array[i];
-	max = max + 1;
-	freq = (int *) malloc(sizeof(int) * (max));
-	sorted = (int *) malloc(sizeof(int) * sz);
-	if (freq == NULL || !sorted)
-		return;
-	for (i = 0; i < max; i++)
-		freq[i] = 0;
-	for (i = 0; i < sz; i++)
-		freq[array[i]] += 1;
-	for (i = 1; i < max; i++)
-		freq[i] += freq[i - 1];
-	for (i = 0; i < max; i++)
+	if (!array || size == 1)
+	return;
+
+	count = malloc(sizeof(int) * len + 1);
+
+	if (!count)
 	{
-		if (i != 0)
-			printf(", ");
-		printf("%d", freq[i]);
+		free(count);
+		return;
 	}
-	for (i = 0; i < sz; i++)
+
+	for (i = 0; i <= len; i++)
+		count[i] = 0;
+
+	for (i = 0; i < (int)size; i++)
+		count[array[i]]++;
+
+	for (i = 1; i <= len; i++)
+		count[i] = count[i] + count[i - 1];
+
+	print_array(count, len + 1);
+	array2 = malloc(sizeof(int) * size);
+
+	if (!array2)
 	{
-		idx = freq[array[i]];
-		sorted[idx - 1] = array[i];
-		freq[array[i]] -= 1;
+		free(array2);
+		return;
 	}
-	for (i = 0; i < sz; i++)
-		array[i] = sorted[i];
-	free(freq);
-	free(sorted);
+
+	for (i = size - 1; i >= 0; i--)
+	{
+		count[array[i]]--;
+		array2[count[array[i]]] = array[i];
+	}
+
+	for (i = 0; i < (int)size; i++)
+		array[i] = array2[i];
+
+	free(count);
+	free(array2);
+}
+
+/**
+ * largest - find the largest element in the array
+ *
+ * @array: the array
+ * @size: size of the array
+ *
+ * Return: largest number
+*/
+
+int largest(int *array, size_t size)
+{
+	int i = 0, len = 0;
+
+	for (i = 0; i < (int)size; i++)
+	{
+		if (array[i] > len)
+		len = array[i];
+	}
+	return (len);
 }
